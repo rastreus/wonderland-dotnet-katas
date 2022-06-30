ec9418f406cbde864ade678ed31551758f4ee7d8
Russell Dillin <rastreus@gmail.com>
2022-06-30 15:08:29 -0700

    Added csharp/doublets

---
+++ csharp/doublets/src/solver.cs
@@ -0,0 +1,10 @@
+﻿namespace doublets;
+
+public class solver {
+
+    public static string[] words() =>
+        System.IO.File.ReadAllLines("./../resources/words.txt");
+
+    public static string[] doublets(string word1, string word2) =>
+        new string[] { "make me work" };
+}
---
+++ csharp/doublets/test/solver-test.cs
@@ -0,0 +1,34 @@
+﻿namespace doublets;
+
+using Xunit;
+using static doublets.solver;
+
+public class solver_test {
+    [Fact]
+    public void with_word_links_found() {
+        Assert.Equal (
+            new string[] { "head", "heal", "teal", "tell", "tall", "tail" },
+            doublets("head", "tail")
+        );
+        Assert.Equal (
+            new string[] { "door", "boor", "book", "look", "lock" },
+            doublets("door", "lock")
+        );
+        Assert.Equal (
+            new string[] { "bank", "bonk", "book", "look", "loon", "loan" },
+            doublets("bank", "loan")
+        );
+        Assert.Equal (
+            new string[] { "wheat", "cheat", "cheap", "cheep", "creep", "creed", "breed", "bread" },
+            doublets("wheat", "bread")
+        );
+    }
+
+    [Fact]
+    public void with_no_word_links_found() {
+        Assert.Equal (
+            new string [] {},
+            doublets("ye", "freezer")
+        );
+    }
+}
